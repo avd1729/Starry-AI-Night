4c8620e80c21998b8c10a22fd8428afa5956725f
avd <aravind19112003@gmail.com>
2024-03-07 12:52:39 +0530

    Cookie Cutter Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Vincent van Gogh's paintings are renowned for their distinctive style,',
+    author='Aravind.M.S',
+    license='MIT',
+)
